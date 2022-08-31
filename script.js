afe6480a914c14f30ec4f43ecec682e3e17c30c3
slavekrouta <97255074+slavekrouta@users.noreply.github.com>
2022-08-31 15:39:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function odpovez() {
     vstup = document.getElementById("vstup").value;
-    if (vstup == "Ahoj") { ahoj(); }
+    if (vstup == "Ahoj" || vstup == "ahoj" || vstup == "Ahoj." || vstup == "ahoj.") { ahoj(); }
+    else if (vstup == "dement" || vstup == "Dement" || vstup == "dement." || vstup == "Dement.") { dement(); }
+    else { alert("cooming soon"); }
 }
 
 function ahoj() {
@@ -26,6 +28,36 @@ function ahoj() {
                     if (variabel == 5) {
                         document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br>Ty: " + vstup;
                         document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br><b>ROBOT: Cau. Jak je?</b>";
+                    }
+                }
+            }
+        }
+    }
+}
+
+function dement() {
+    variabel = Math.floor(Math.random() * 5) + 1;
+    if (variabel == 1) {
+        document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br>Ty: " + vstup;
+        document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br><b>ROBOT: ???</b>";
+    }
+    else {
+        if (variabel == 2) {
+            document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br>Ty: " + vstup;
+            document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br><b>ROBOT: thanks</b>";
+        }
+        else {
+            if (variabel == 3) {
+                document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br>Ty: " + vstup;
+                document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br><b>ROBOT: Ses ty</b>";
+            } else {
+                if (variabel == 4) {
+                    document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br>Ty: " + vstup;
+                    document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br><b>ROBOT: </b><img src=\"https://i.ytimg.com/vi/hEj8am0LTE0/maxresdefault.jpg\" style=\"width:320px;height:180px;\">";
+                } else {
+                    if (variabel == 5) {
+                        document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br>Ty: " + vstup;
+                        document.getElementById("odpovedi").innerHTML = document.getElementById("odpovedi").innerHTML + "<br><b>ROBOT: To jako ze ja jsem dement?! >:(</b>";
                     }
                 }
             }
